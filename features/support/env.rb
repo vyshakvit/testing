c2e37daddbc081d5d19273525cbd0b5af6c8694e
Vyshak <vyshakk@vitinfotech.com>
2020-01-22 15:36:33 +0530

    test

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require 'capybara/rails'
 require 'capybara/cucumber'
 require 'capybara/spec/test_app'
 
@@ -11,11 +12,16 @@ Capybara.app = TestApp
 # These drivers are only used for testing driver switching.
 # They don't actually need to process javascript so use RackTest
 
-Capybara.register_driver :javascript_test do |app|
-  Capybara::RackTest::Driver.new(app)
-end
+# Capybara.register_driver :javascript_test do |app|
+#   Capybara::RackTest::Driver.new(app)
+# end
 
-Capybara.javascript_driver = :javascript_test
+Capybara.javascript_driver = :webkit
+
+Capybara::Webkit.configure do |config|
+  config.allow_unknown_urls
+  # config.ignore_ssl_errors
+end
 
 Capybara.register_driver :named_test do |app|
   Capybara::RackTest::Driver.new(app)
